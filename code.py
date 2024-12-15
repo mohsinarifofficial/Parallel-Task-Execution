2a350ae4bbae1ba629f28450fb086a1cce153269
mohsinarifofficial <140285023+mohsinarifofficial@users.noreply.github.com>
2024-12-15 22:30:42 +0500

    Update code.py

--- code.py
+++ code.py
@@ -26,3 +26,28 @@ csv_file=read("new.csv")
 
 for i in csv_file:
     print(i)
+
+# pdf
+import PyPDF2
+from fpdf import FPDF
+def read_pdf(file_path):
+    file=open(file_path,"rb")
+    reader = PyPDF2.PdfReader(file)
+    text = ""
+    # Iterate through each page and extract text
+    for page in reader.pages:
+        text += page.extract_text()
+    return text
+
+def write(file_path,data):
+    data=data.encode("latin-1","ignore").decode("latin-1")
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.multi_cell(0, 10, data)
+    pdf.output(file_path)
+
+text = read_pdf("3_1_8_Gajski_137-147_accepted_31.10.2023.pdf")
+print(text)
+write("my.pdf",text)
+
